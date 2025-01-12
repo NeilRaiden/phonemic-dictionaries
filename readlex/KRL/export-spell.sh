031f05e11231f44611a119bb1bdfc4fa081a5356
NeilRaiden <NeilRaidenLLC@gmail.com>
2025-01-12 16:18:53 -0500

    delete export-spell.sh

--- readlex/KRL/export-spell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# export Shavian column:
-awk -F'\t' '{print $3}' KRL-US.tsv  >en_US.shav
-awk -F'\t' '{print $3}' KRL-UK.tsv  >en_UK.shav
-
-# export IPA column:
-awk -F'\t' '{print $2}' KRL-US.tsv  >en_US.ipa
-awk -F'\t' '{print $2}' KRL-UK.tsv  >en_UK.ipa
